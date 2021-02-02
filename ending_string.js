2f0e22fc6ce22d7c965f54a5fccd42e96699a68a
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2021-02-02 13:51:39 +0100

    Update ending_string.js

--- ending_string.js
+++ ending_string.js
@@ -1,4 +1,4 @@
-/* onfirm the Ending
+/* Confirm the Ending
 Check if a string (first argument, str) ends with the given target string (second argument, target).
 
 This challenge can be solved with the .endsWith() method, which was introduced in ES2015.
