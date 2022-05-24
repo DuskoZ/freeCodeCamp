c5ea4e182ad7e655749fcc67771f9d54ffdd6fc9
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-05-24 17:09:55 +0200

    changed audio clip urls

--- Front End Development Libraries/build-a-drum-machine/src/App.js
+++ Front End Development Libraries/build-a-drum-machine/src/App.js
@@ -6,55 +6,55 @@ const bankOne = [
         keyCode: 81,
         keyTrigger: "Q",
         id: "Heater-1",
-        url: "./sounds/bank-one/Heater-1.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
     },
     {
         keyCode: 87,
         keyTrigger: "W",
         id: "Heater-2",
-        url: "./sounds/bank-one/Heater-2.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
     },
     {
         keyCode: 69,
         keyTrigger: "E",
         id: "Heater-3",
-        url: "./sounds/bank-one/Heater-3.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
     },
     {
         keyCode: 65,
         keyTrigger: "A",
         id: "Heater-4",
-        url: "./sounds/bank-one/Heater-4_1.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
     },
     {
         keyCode: 83,
         keyTrigger: "S",
         id: "Clap",
-        url: "./sounds/bank-one/Heater-6.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
     },
     {
         keyCode: 68,
         keyTrigger: "D",
         id: "Open-HH",
-        url: "./sounds/bank-one/Dsc_Oh.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
     },
     {
         keyCode: 90,
         keyTrigger: "Z",
         id: "Kick-n'-Hat",
-        url: "./sounds/bank-one/Kick_n_Hat.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
     },
     {
         keyCode: 88,
         keyTrigger: "X",
         id: "Kick",
-        url: "./sounds/bank-one/RP4_KICK_1.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
     },
     {
         keyCode: 67,
         keyTrigger: "C",
         id: "Closed-HH",
-        url: "./sounds/bank-one/Cev_H2.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
     },
 ];
 
@@ -63,55 +63,55 @@ const bankTwo = [
         keyCode: 81,
         keyTrigger: "Q",
         id: "Chord-1",
-        url: "./sounds/bank-two/Chord_1.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
     },
     {
         keyCode: 87,
         keyTrigger: "W",
         id: "Chord-2",
-        url: "./sounds/bank-two/Chord_2.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
     },
     {
         keyCode: 69,
         keyTrigger: "E",
         id: "Chord-3",
-        url: "./sounds/bank-two/Chord_3.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
     },
     {
         keyCode: 65,
         keyTrigger: "A",
         id: "Shaker",
-        url: "./sounds/bank-two/Give_us_a_light.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
     },
     {
         keyCode: 83,
         keyTrigger: "S",
         id: "Open-HH",
-        url: "./sounds/bank-two/Dry_Ohh.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
     },
     {
         keyCode: 68,
         keyTrigger: "D",
         id: "Closed-HH",
-        url: "./sounds/bank-two/Bld_H1.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
     },
     {
         keyCode: 90,
         keyTrigger: "Z",
         id: "Punchy-Kick",
-        url: "./sounds/bank-two/punchy_kick_1.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
     },
     {
         keyCode: 88,
         keyTrigger: "X",
         id: "Side-Stick",
-        url: "./sounds/bank-two/side_stick_1.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
     },
     {
         keyCode: 67,
         keyTrigger: "C",
         id: "Snare",
-        url: "./sounds/bank-two/Brk_Snr.mp3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
     },
 ];
 
@@ -191,9 +191,7 @@ class App extends React.Component {
                 ? { float: "left" }
                 : { float: "right" };
 
-        const clips = [].slice.call(
-            document.getElementsByClassName("audio-clip")
-        );
+        const clips = [].slice.call(document.getElementsByClassName("clip"));
         clips.forEach((clip) => {
             clip.volume = this.state.sliderValue;
         });
