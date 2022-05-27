646d88859434f9e590023dbf98c32d8fa08b47db
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-05-27 22:17:19 +0200

    updated component

--- Front End Development Libraries/build-a-javascript-calculator/src/App.js
+++ Front End Development Libraries/build-a-javascript-calculator/src/App.js
@@ -1,9 +1,29 @@
-function App() {
-    return (
-        <div className="app">
-            <h1>JavaScript Calculator</h1>
-        </div>
-    );
+import React from "react";
+import Buttons from "./components/Buttons";
+import Formula from "./components/Formula";
+import Result from "./components/Result";
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentValue: "0",
+            prevValue: "0",
+            formula: "",
+            currentSign: "pos",
+            lastClicked: "",
+        };
+    }
+
+    render() {
+        return (
+            <div className="calculator">
+                <Formula />
+                <Result />
+                <Buttons />
+            </div>
+        );
+    }
 }
 
-export default App;
+export default Calculator;
