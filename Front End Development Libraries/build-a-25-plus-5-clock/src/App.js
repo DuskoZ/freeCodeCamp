bba869c4fe2fb2b9a2675534a699a092a337a594
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-06-01 14:40:58 +0200

    test rendering

--- Front End Development Libraries/build-a-25-plus-5-clock/src/App.js
+++ Front End Development Libraries/build-a-25-plus-5-clock/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Break from "./components/Break";
+import Controls from "./components/Controls";
+import Session from "./components/Session";
+import Timer from "./components/Timer";
 
 function App() {
-    return <div className="app">Clock app</div>;
+    return (
+        <div className="app">
+            <h1 className="title">Timer Clock</h1>
+
+            <div className="length-container">
+                <Break />
+                <Session />
+            </div>
+            <Timer />
+            <Controls />
+        </div>
+    );
 }
 
 export default App;
