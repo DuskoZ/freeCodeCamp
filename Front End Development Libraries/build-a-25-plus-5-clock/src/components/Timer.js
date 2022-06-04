e2478495993113e995aa5ade148496a961c0a831
DuskoZ <77397182+DuskoZ@users.noreply.github.com>
2022-06-04 21:19:20 +0200

    updated component

--- Front End Development Libraries/build-a-25-plus-5-clock/src/components/Timer.js
+++ Front End Development Libraries/build-a-25-plus-5-clock/src/components/Timer.js
@@ -1,8 +1,15 @@
 const Timer = (props) => {
+    let minutes = Math.floor(props.clockCount / 60);
+    let seconds = props.clockCount % 60;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
     return (
         <div className="timer-container">
             <h2 id="timer-label">{props.currentTimer}</h2>
-            <p id="time-left">25:00</p>
+            <p id="time-left">
+                {minutes}:{seconds}
+            </p>
         </div>
     );
 };
